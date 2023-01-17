9d8c2a4a07a9ac8b7dbc113cca6d4a4dc44a09b6
Celeste Wouters <perso@elementw.net>
2023-01-17 21:07:58 +0100

    v1.0.2, always show resource pack

--- src/main/java/net/elementw/fullblockchests/DefaultResources.java
+++ src/main/java/net/elementw/fullblockchests/DefaultResources.java
@@ -2,8 +2,6 @@ package net.elementw.fullblockchests;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -26,16 +24,10 @@ import java.util.function.Predicate;
 
 public class DefaultResources implements ResourcePack, ClientModInitializer {
     private static final LiteralText NAME = new LiteralText(ModConfig.MOD_NAME);
-    private static final LiteralText DESCRIPTION = new LiteralText("Internal " + ModConfig.MOD_NAME + " resources");
+    private static final LiteralText DESCRIPTION = new LiteralText("Keep this above Fabric");
 
     @Override
     public void onInitializeClient() {
-        // Nostalgic Tweaks messes up our block models even if the chest tweaks are disabled.
-        // Bypass this by creating a fake resource pack to load our resources after NT.
-        if (!FabricLoader.getInstance().isModLoaded("nostalgic_tweaks")) {
-            return;
-        }
-        ModConfig.LOGGER.info("Found Nostalgic Tweaks, creating fake resource pack to override its chest models");
         ResourcePackManager manager = MinecraftClient.getInstance().getResourcePackManager();
         ResourcePackProvider fbcProvider = (consumer, factory) -> {
             consumer.accept(factory.create(
