8ae68ac1fc8376320050294420845a857c939033
Celeste Wouters <perso@elementw.net>
2023-01-20 00:05:28 +0100

    v1.0.3 Fix wrongly overriding default MC assets, change block model path

--- src/main/java/net/elementw/fullblockchests/DefaultResources.java
+++ src/main/java/net/elementw/fullblockchests/DefaultResources.java
@@ -5,12 +5,12 @@ import com.google.common.collect.Sets;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.resource.*;
 import net.minecraft.resource.metadata.PackResourceMetadata;
 import net.minecraft.resource.metadata.ResourceMetadataReader;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
 import java.io.FileNotFoundException;
@@ -23,8 +23,11 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 public class DefaultResources implements ResourcePack, ClientModInitializer {
+    private static final ResourcePackSource RESOURCE_PACK_SOURCE = packName ->
+            new TranslatableText("pack.nameAndSource", packName, new TranslatableText("pack.source.fabricmod"));
     private static final LiteralText NAME = new LiteralText(ModConfig.MOD_NAME);
-    private static final LiteralText DESCRIPTION = new LiteralText("Keep this above Fabric");
+    private static final LiteralText DESCRIPTION = new LiteralText("Keep this above other mods");
+    private static final String RESOURCE_PACK_ROOT = "assets/" + ModConfig.MOD_ID + "/resourcepack/";
 
     @Override
     public void onInitializeClient() {
@@ -37,12 +40,13 @@ public class DefaultResources implements ResourcePack, ClientModInitializer {
                     () -> this,
                     new PackResourceMetadata(DESCRIPTION, 8),
                     ResourcePackProfile.InsertionPosition.TOP,
-                    ResourcePackSource.PACK_SOURCE_BUILTIN));
+                    RESOURCE_PACK_SOURCE));
         };
+        ModConfig.LOGGER.debug("ResourcePackManager provider type is {}", manager.providers.getClass().getName());
         try {
             manager.providers.add(fbcProvider);
         } catch (UnsupportedOperationException e) {
-            ModConfig.LOGGER.info("Making ResourcePackManager.providers mutable");
+            ModConfig.LOGGER.info("Making ResourcePackManager.providers into mutable HashSet");
             manager.providers = Sets.newHashSet(manager.providers);
             manager.providers.add(fbcProvider);
         }
@@ -57,17 +61,15 @@ public class DefaultResources implements ResourcePack, ClientModInitializer {
         if (fileName.equals("pack.png")) {
             return getClass().getClassLoader().getResourceAsStream("assets/" + ModConfig.MOD_ID + "/icon.png");
         }
+        fileName = RESOURCE_PACK_ROOT + fileName;
         InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
         if (is == null) throw new FileNotFoundException(fileName);
         return is;
     }
 
     private URL url(ResourceType type, Identifier id) {
-        return url(type.getDirectory()+"/"+id.getNamespace()+"/"+id.getPath());
-    }
-
-    private URL url(String path) {
-        return getClass().getClassLoader().getResource(path);
+        return getClass().getClassLoader().getResource(
+                RESOURCE_PACK_ROOT + type.getDirectory() + "/" + id.getNamespace() + "/" + id.getPath());
     }
 
     @Override
